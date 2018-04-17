c9606f802f9970c782371b3bf99c517b3c96a061
Unknown <georgu.dimitrov@gmail.com>
2018-04-17 13:35:59 +0300

    Initial

---
+++ ITest/ITest.Data.Models/Question.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ITest.Data.Models
+{
+    public class Question
+    {
+    }
+}
---
+++ ITest/ITest.Data.Models/Test.cs
@@ -0,0 +1,22 @@
+﻿using ITest.Data.Models.Abstraction;
+using ITest.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ITest.Data.Models
+{
+    public class Test : DataModel
+    {
+        public Test()
+        {
+            this.Questions = new HashSet<Question>();
+        }
+        public string Name { get; set; } 
+
+        public string AuthorID { get; set; }
+        public User Author { get; set; }
+
+        public ICollection<Question> Questions { get; set; }
+    }
+}
