bc3e3df0fb1552c66f34c25706607fc1937af6d4
iskraL <iskra.lumbeva@gmail.com>
2018-04-20 13:46:07 +0300

    Added repository and mapping provider(automapper)

---
+++ ITest/ITest.Data/Repository/EfRepository.cs
@@ -0,0 +1,70 @@
+﻿using ITest.Data.Models.Abstraction;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System;
+using System.Linq;
+
+namespace ITest.Data.Repository
+{
+    public class EfRepository<T> : IRepository<T>
+        where T : class, IDeletable
+    {
+        private readonly ApplicationDbContext context;
+
+        public EfRepository(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public IQueryable<T> All
+        {
+            get
+            {
+                return this.context.Set<T>().Where(x => !x.IsDeleted);
+            }
+        }
+
+        public IQueryable<T> AllAndDeleted
+        {
+            get
+            {
+                return this.context.Set<T>();
+            }
+        }
+
+        public void Add(T entity)
+        {
+            EntityEntry entry = this.context.Entry(entity);
+
+            if (entry.State != EntityState.Detached)
+            {
+                entry.State = EntityState.Added;
+            }
+            else
+            {
+                this.context.Set<T>().Add(entity);
+            }
+        }
+
+        public void Delete(T entity)
+        {
+            entity.IsDeleted = true;
+            entity.DeletedOn = DateTime.Now;
+
+            var entry = this.context.Entry(entity);
+            entry.State = EntityState.Modified;
+        }
+
+        public void Update(T entity)
+        {
+            EntityEntry entry = this.context.Entry(entity);
+            if (entry.State == EntityState.Detached)
+            {
+                this.context.Set<T>().Attach(entity);
+            }
+
+            entry.State = EntityState.Modified;
+        }
+
+    }
+}
---
+++ ITest/ITest.Data/Saver/EntitySaver.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ITest.Data.Saver
+{
+    public class EntitySaver : ISaver
+    {
+        private readonly ApplicationDbContext context;
+
+        public EntitySaver(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        public void SaveChanges()
+        {
+            this.context.SaveChanges();
+        }
+        public async void SaveChangesAsync()
+        {
+            await this.context.SaveChangesAsync();
+        }
+    }
+}
